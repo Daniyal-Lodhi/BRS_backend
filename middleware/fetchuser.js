d2254ddda2df48c1a6cf99ffbb116e3301f599bb
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-07-13 18:31:07 +0500

    .....

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -8,7 +8,7 @@ const fetchuser = (req,res,next)=>{
         res.status(401).send({'error': "token not found"})
     } 
     try{
-        const data = jwt.verify(token,"It has been a tough ride") ;
+        const data = jwt.verify(token,process.env.SECRET) ;
         req.customer = data.customer ;
         next()
     }catch(error){
