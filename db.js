294aecf081149468d7e1886a0ebfb1a76dde7b7a
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-09-19 18:19:40 +0500

    .

--- db.js
+++ db.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config() 
  
 
-
+ 
 var connection = mysql.createConnection({   
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE ,
-    port: process.env.PORT, 
+    host: "brs-bikerentalsystem.aivencloud.com",
+    user: "avnadmin",
+    password: 'AVNS_Dx7Y59huxYYUyJD4ipH',
+    database: 'brs' ,
+    port: '23552', 
     ssl: {
         rejectUnauthorized: false, // This line allows self-signed certificates, remove it if you have a valid SSL certificate
       },
